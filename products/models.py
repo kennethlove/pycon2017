c475c2d3c6125dee46aab447652ffec1e957c762
Kenneth Love <kenneth@gigantuan.net>
2017-04-15 14:30:29 -0700

    Product model.
    references #1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True, default='')
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    location = models.CharField(max_length=10, unique=True)
+    serial_number = models.CharField(max_length=40, unique=True)
+    quantity = models.IntegerField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
