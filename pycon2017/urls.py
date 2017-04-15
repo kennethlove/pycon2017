290dec5f7f22e298708cb1458b1d3709e7c17d66
Kenneth Love <kenneth@gigantuan.net>
2017-04-15 15:09:31 -0700

    base layout
    ref #7
    ref #8

--- pycon2017/urls.py
+++ pycon2017/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^admin/', admin.site.urls),
 ]
