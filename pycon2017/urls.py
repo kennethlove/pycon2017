3ceeb78c6d3833c2712fc462b1289f83dfc2e288
Kenneth Love <kenneth@gigantuan.net>
2017-04-15 16:34:24 -0700

    serve uploaded media in development

--- pycon2017/urls.py
+++ pycon2017/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
@@ -23,3 +26,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^store/', include('products.urls', namespace='products')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
