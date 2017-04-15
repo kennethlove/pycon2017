03126a52a64939767a97bff4ea6c5d7acef21bdb
Kenneth Love <kenneth@gigantuan.net>
2017-04-15 16:55:33 -0700

    Purchase model.
    closes #5

--- customers/models.py
+++ customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from django_countries.fields import CountryField
 
@@ -15,3 +16,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.email})'
+
+
+class Purchase(models.Model):
+    customer = models.ForeignKey(Customer, related_name='purchases')
+    place_at = models.DateTimeField(default=timezone.now)
+    shipped_at = models.DateTimeField(blank=True, null=True)
+    discount_code = models.CharField(blank=True, default='', max_length=20)
+    total = models.DecimalField(max_digits=9, decimal_places=2)
+    shipped = models.BooleanField(default=False)
