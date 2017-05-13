4137984fef874c2e0141e67632abc4373341c170
Kenneth Love <kenneth@gigantuan.net>
2017-05-13 13:42:02 -0700

    models registered

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Customer)
+admin.site.register(models.Purchase)
+admin.site.register(models.PurchaseItem)
--- products/admin.py
+++ products/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 
+from . import models
+
+admin.site.register(models.Product)
+admin.site.register(models.Image)
+admin.site.register(models.Category)
