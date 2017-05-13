9fad1ac661dd41ddadae0d9cf71fc3bfc4870de1
Kenneth Love <kenneth@gigantuan.net>
2017-05-13 15:26:11 -0700

    custom action

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.utils import timezone
 
 from . import models
 
@@ -25,8 +26,17 @@ class CustomerAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+def ship(modeladmin, request, queryset):
+    queryset.update(
+        shipped=True,
+        shipped_at=timezone.now()
+    )
+ship.short_description = 'Mark purchases as shipped now'
+
+
 @admin.register(models.Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
+    actions = [ship]
     list_display = ['customer', 'placed_at', 'shipped_at', 'shipped', 'total']
     list_editable = ['shipped']
     list_filter = ['shipped', 'placed_at', 'shipped_at', BigOrderFilter]
