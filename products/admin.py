e9fbd7b281f6efcd3601a33ff5d1d7d9bf195d08
Kenneth Love <kenneth@gigantuan.net>
2017-05-13 15:37:51 -0700

    fields let us cut out fields

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Product)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+
 admin.site.register(models.Image)
 admin.site.register(models.Category)
